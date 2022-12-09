fe382e7925850482bd221b953de17cf207b7403c
fayscys <onikoyitinuade0526@gmail.com>
2022-12-09 07:07:12 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int  main(void)
+{
+
+          puts("\"programming is like building a multilingual puzzle");
+	  return (0);
+}	  
