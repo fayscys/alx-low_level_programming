9a6bc2dc4f436114345b79e6820208632d50d10a
fayscys <onikoyitinuade0526@gmail.com>
2022-12-31 15:35:35 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("size of a char: %zu bytes(s)\n", sizeof(char));
 printf("size of an int: %zu bytes(s)\n", sizeof(int));
 printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("size of a long long int: %zu bytes(s)\n", sizeof(long long int));
-printf("size of a float %zu byte(s)\n", sizeof(float));
+printf("size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
